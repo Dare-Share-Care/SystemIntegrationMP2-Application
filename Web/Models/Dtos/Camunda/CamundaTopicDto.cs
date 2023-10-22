c0a563754459c24a532e0ffad332a312091348f7
FrederikBA <frederikbandersen98@gmail.com>
2023-10-22 13:56:23 +0200

    dto nullables

--- Web/Models/Dtos/Camunda/CamundaTopicDto.cs
+++ Web/Models/Dtos/Camunda/CamundaTopicDto.cs
@@ -5,9 +5,9 @@ namespace Web.Models.Dtos;
 public class CamundaTopicDto
 {
     [JsonPropertyName("topicName")]
-    public string TopicName { get; set; }
+    public string? TopicName { get; set; }
     [JsonPropertyName("lockDuration")]
     public int LockDuration { get; set; }
     [JsonPropertyName("variables")]
-    public List<string> Variables { get; set; }
+    public List<string>? Variables { get; set; }
 }
--- Web/Models/Dtos/Camunda/CompleteCamundaExternalTaskDto.cs
+++ Web/Models/Dtos/Camunda/CompleteCamundaExternalTaskDto.cs
@@ -5,9 +5,9 @@ namespace Web.Models.Dtos;
 public class CompleteCamundaExternalTaskDto : BaseDto
 {
     [JsonPropertyName("workerId")]
-    public string WorkerId { get; set; }
+    public string? WorkerId { get; set; }
     [JsonPropertyName("variables")]
     public Dictionary<string, Dictionary<string, object>>? Variables { get; set; }
     [JsonPropertyName("topic")]
-    public CamundaTopicDto Topic { get; set; }
+    public CamundaTopicDto? Topic { get; set; }
 }
